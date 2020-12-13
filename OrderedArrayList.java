95eb8e5621059ecc449c0b4c77d98f28f96776a3
adnanq2004 <aquayyum20@stuy.edu>
2020-12-13 18:21:02 -0500

    fixed a possible error to come

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,8 +9,8 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 	}
 
 	public boolean add(T element) {
-		int place = 0;
-		for (int i = 0; i < this.size() && place == 0; i++) {
+		int place = -1000;
+		for (int i = 0; i < this.size() && place == -1000; i++) {
 			if (this.get(i).compareTo(element) > 0) {
 				place = i;
 			}
